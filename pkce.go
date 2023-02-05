f9f75c43fa317b1f3a1b5814f273cb98ca4b3eae
ras0q <ras0q@users.noreply.github.com>
2023-02-06 07:43:57 +0900

    :recycle: move funcs about pkce

--- pkce.go
+++ pkce.go
@@ -2,7 +2,11 @@ package traqoauth2
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
+
+	"golang.org/x/oauth2"
 )
 
 // GenerateCodeVerifier generates a code verifier.
@@ -13,4 +17,59 @@ func GenerateCodeVerifier() string {
 		panic(err)
 	}
 	return base64.RawURLEncoding.EncodeToString(b)
+}
+
+// CodeChallengeMethod represents the code challenge method.
+type CodeChallengeMethod string
+
+const (
+	// CodeChallengePlain is the PKCE "plain" method.
+	CodeChallengePlain CodeChallengeMethod = "plain"
+	// CodeChallengeS256 is the PKCE "S256" method.
+	CodeChallengeS256 CodeChallengeMethod = "S256"
+)
+
+// String returns the string representation of the code challenge method.
+func (m CodeChallengeMethod) String() string {
+	if m == CodeChallengePlain || m == CodeChallengeS256 {
+		return string(m)
+	}
+
+	fmt.Printf("WARN: unavailable code challenge method: %s\n", string(m))
+
+	return ""
+}
+
+// GenerateCodeChallenge generates the code challenge from the code verifier.
+// Ref: https://datatracker.ietf.org/doc/html/rfc7636#section-4.2
+func (m CodeChallengeMethod) GenerateCodeChallenge(codeVerifier string) string {
+	switch m {
+	case CodeChallengePlain:
+		return codeVerifier
+	case CodeChallengeS256:
+		h := sha256.Sum256([]byte(codeVerifier))
+		return base64.RawURLEncoding.EncodeToString(h[:])
+	default:
+		fmt.Printf("WARN: unavailable code challenge method: %s\n", string(m))
+
+		return ""
+	}
+}
+
+// WithCodeChallenge sets the code_challenge parameter.
+func WithCodeChallenge(codeChallenge string) oauth2.AuthCodeOption {
+	return oauth2.SetAuthURLParam("code_challenge", codeChallenge)
+}
+
+// WithCodeChallengeMethod sets the code_challenge_method parameter.
+// The default value is "plain".
+// If you want to use "S256", use WithCodeChallengeMethod(traqoauth2.CodeChallenge).
+func WithCodeChallengeMethod(codeChallengeMethod CodeChallengeMethod) oauth2.AuthCodeOption {
+	return oauth2.SetAuthURLParam("code_challenge_method", codeChallengeMethod.String())
+}
+
+// WithCodeVerifier sets the code_verifier parameter.
+// If you had use WithCodeChallenge, you also must use this.
+func WithCodeVerifier(codeVerifier string) oauth2.AuthCodeOption {
+	return oauth2.SetAuthURLParam("code_verifier", codeVerifier)
 }
