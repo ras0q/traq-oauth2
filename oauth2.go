2bc04333478ba8594301178e8e7bb6f8fc66ff53
ras0q <ras0q@users.noreply.github.com>
2023-02-06 02:38:02 +0900

    :boom: add type CodeChallengeMethod & change WithCodeChallengeMethod arg

--- oauth2.go
+++ oauth2.go
@@ -1,6 +1,9 @@
 package traqoauth2
 
 import (
+	"encoding/base64"
+	"fmt"
+
 	"golang.org/x/oauth2"
 )
 
@@ -34,13 +37,48 @@ func WithCodeChallenge(codeChallenge string) oauth2.AuthCodeOption {
 
 // WithCodeChallengeMethod sets the code_challenge_method parameter.
 // The default value is "plain".
-// If you want to use "S256", use WithCodeChallengeMethod("S256").
-func WithCodeChallengeMethod(codeChallengeMethod string) oauth2.AuthCodeOption {
-	return oauth2.SetAuthURLParam("code_challenge_method", codeChallengeMethod)
+// If you want to use "S256", use WithCodeChallengeMethod(traqoauth2.CodeChallenge).
+func WithCodeChallengeMethod(codeChallengeMethod CodeChallengeMethod) oauth2.AuthCodeOption {
+	return oauth2.SetAuthURLParam("code_challenge_method", codeChallengeMethod.String())
 }
 
 // WithCodeVerifier sets the code_verifier parameter.
 // If you had use WithCodeChallenge, you also must use this.
 func WithCodeVerifier(codeVerifier string) oauth2.AuthCodeOption {
 	return oauth2.SetAuthURLParam("code_verifier", codeVerifier)
+}
+
+// CodeChallengeMethod represents the code challenge method.
+type CodeChallengeMethod string
+
+const (
+	// CodeChallengePlain is the PKCE "plain" method.
+	CodeChallengePlain CodeChallengeMethod = "plain"
+	// CodeChallengeS256 is the PKCE "S256" method.
+	CodeChallengeS256 CodeChallengeMethod = "S256"
+)
+
+// String returns the string representation of the code challenge method.
+func (m CodeChallengeMethod) String() string {
+	if m == CodeChallengePlain || m == CodeChallengeS256 {
+		return string(m)
+	}
+
+	fmt.Printf("WARN: unavailable code challenge method: %s\n", string(m))
+
+	return ""
+}
+
+// GenerateCodeChallenge generates the code challenge from the code verifier.
+func (m CodeChallengeMethod) GenerateCodeChallenge(codeVerifier string) string {
+	switch m {
+	case CodeChallengePlain:
+		return codeVerifier
+	case CodeChallengeS256:
+		return base64.RawURLEncoding.EncodeToString([]byte(codeVerifier))
+	default:
+		fmt.Printf("WARN: unavailable code challenge method: %s\n", string(m))
+
+		return ""
+	}
 }
