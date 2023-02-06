d6b28f0f1ed4e1ccadc17042165b2ec7ba6a233f
ras0q <ras0q@users.noreply.github.com>
2023-02-06 10:20:33 +0900

    :boom: change NewConfig ret to Config (wrapper of oauth2.Config)

--- oauth2.go
+++ oauth2.go
@@ -10,14 +10,21 @@ var TraQ = oauth2.Endpoint{
 	TokenURL: "https://q.trap.jp/api/v3/oauth2/token",
 }
 
+// Config is a wrapper of oauth2.Config.
+type Config struct {
+	*oauth2.Config
+}
+
 // NewConfig returns a new oauth2.Config for traQ.
-func NewConfig(clientID string, redirectURL string, opts ...func(*oauth2.Config)) *oauth2.Config {
-	c := &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: "",
-		Endpoint:     TraQ,
-		RedirectURL:  redirectURL,
-		Scopes:       []string{},
+func NewConfig(clientID string, redirectURL string, opts ...func(*Config)) *Config {
+	c := &Config{
+		Config: &oauth2.Config{
+			ClientID:     clientID,
+			ClientSecret: "",
+			Endpoint:     TraQ,
+			RedirectURL:  redirectURL,
+			Scopes:       []string{},
+		},
 	}
 
 	for _, opt := range opts {
